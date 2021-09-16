dd9014149f6cb1de1bc978941e7d6768fbdb9a1f
Govind Kumar <govind.kumar@i.suprdaily.com>
2021-09-16 23:52:11 +0530

    removed nice scroll

--- js/custom.js
+++ js/custom.js
@@ -18,91 +18,91 @@
     arrow: true,
   });
 
-  $(document).ready(function () {
-    /*! Nicescroll */
+  // $(document).ready(function () {
+  //   /*! Nicescroll */
 
-    $("#boxscroll-1").niceScroll("#boxscroll-in-1", {
-      cursorcolor: "#000",
-      cursoropacitymax: 0.5,
-      cursorwidth: "6px",
-      cursorborder: "0px solid #fff",
-      dblclickzoom: false,
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      autohidemode: false,
-      bouncescroll: false,
-      railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
-    });
-    $("#boxscroll-2").niceScroll("#boxscroll-in-2", {
-      cursorcolor: "#000",
-      cursoropacitymax: 0.5,
-      cursorwidth: "6px",
-      cursorborder: "0px solid #fff",
-      dblclickzoom: false,
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      autohidemode: false,
-      bouncescroll: false,
-      railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
-    });
-    $("#boxscroll-3").niceScroll("#boxscroll-in-3", {
-      cursorcolor: "#000",
-      cursoropacitymax: 0.5,
-      cursorwidth: "6px",
-      cursorborder: "0px solid #fff",
-      dblclickzoom: false,
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      autohidemode: false,
-      bouncescroll: false,
-      railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
-    });
-    $("#boxscroll-4").niceScroll("#boxscroll-in-4", {
-      cursorcolor: "#000",
-      cursoropacitymax: 0.5,
-      cursorwidth: "6px",
-      cursorborder: "0px solid #fff",
-      dblclickzoom: false,
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      autohidemode: false,
-      bouncescroll: false,
-      railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
-    });
-    $("#boxscroll-5").niceScroll("#boxscroll-in-5", {
-      cursorcolor: "#000",
-      cursoropacitymax: 0.5,
-      cursorwidth: "6px",
-      cursorborder: "0px solid #fff",
-      dblclickzoom: false,
-      scrollspeed: 60,
-      mousescrollstep: 60,
-      autohidemode: false,
-      bouncescroll: false,
-      railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
-    });
+  //   $("#boxscroll-1").niceScroll("#boxscroll-in-1", {
+  //     cursorcolor: "#000",
+  //     cursoropacitymax: 0.5,
+  //     cursorwidth: "6px",
+  //     cursorborder: "0px solid #fff",
+  //     dblclickzoom: false,
+  //     scrollspeed: 60,
+  //     mousescrollstep: 60,
+  //     autohidemode: false,
+  //     bouncescroll: false,
+  //     railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
+  //   });
+  //   $("#boxscroll-2").niceScroll("#boxscroll-in-2", {
+  //     cursorcolor: "#000",
+  //     cursoropacitymax: 0.5,
+  //     cursorwidth: "6px",
+  //     cursorborder: "0px solid #fff",
+  //     dblclickzoom: false,
+  //     scrollspeed: 60,
+  //     mousescrollstep: 60,
+  //     autohidemode: false,
+  //     bouncescroll: false,
+  //     railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
+  //   });
+  //   $("#boxscroll-3").niceScroll("#boxscroll-in-3", {
+  //     cursorcolor: "#000",
+  //     cursoropacitymax: 0.5,
+  //     cursorwidth: "6px",
+  //     cursorborder: "0px solid #fff",
+  //     dblclickzoom: false,
+  //     scrollspeed: 60,
+  //     mousescrollstep: 60,
+  //     autohidemode: false,
+  //     bouncescroll: false,
+  //     railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
+  //   });
+  //   $("#boxscroll-4").niceScroll("#boxscroll-in-4", {
+  //     cursorcolor: "#000",
+  //     cursoropacitymax: 0.5,
+  //     cursorwidth: "6px",
+  //     cursorborder: "0px solid #fff",
+  //     dblclickzoom: false,
+  //     scrollspeed: 60,
+  //     mousescrollstep: 60,
+  //     autohidemode: false,
+  //     bouncescroll: false,
+  //     railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
+  //   });
+  //   $("#boxscroll-5").niceScroll("#boxscroll-in-5", {
+  //     cursorcolor: "#000",
+  //     cursoropacitymax: 0.5,
+  //     cursorwidth: "6px",
+  //     cursorborder: "0px solid #fff",
+  //     dblclickzoom: false,
+  //     scrollspeed: 60,
+  //     mousescrollstep: 60,
+  //     autohidemode: false,
+  //     bouncescroll: false,
+  //     railpadding: { top: 20, right: 5, left: 0, bottom: 20 },
+  //   });
 
-    //Countdown
+  //   //Countdown
 
-    $(".countdown").downCount(
-      {
-        date: "07/27/2019 12:00:00",
-      },
-      function () {
-        // alert('WOOT WOOT, done!');
-      }
-    );
+  //   $(".countdown").downCount(
+  //     {
+  //       date: "07/27/2019 12:00:00",
+  //     },
+  //     function () {
+  //       // alert('WOOT WOOT, done!');
+  //     }
+  //   );
 
-    //Animated Icons
+  //   //Animated Icons
 
-    var options = {
-      duration: 150,
-      type: "oneByOne",
-      animTimingFunction: Vivus.EASE,
-    };
+  //   var options = {
+  //     duration: 150,
+  //     type: "oneByOne",
+  //     animTimingFunction: Vivus.EASE,
+  //   };
 
-    var vivus = new Vivus("svg-ticket", options, onComplete);
+  //   var vivus = new Vivus("svg-ticket", options, onComplete);
 
-    function onComplete() {}
-  });
+  //   function onComplete() {}
+  // });
 })(jQuery);
