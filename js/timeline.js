9449651f369b8a6fa55a5f1e1b9c224de17f6ade
Anubhav Dixit <anubhavdxt46@gmail.com>
2021-09-19 19:08:55 +0530

    Update index.html and timeline.js for the new timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,6 +1,20 @@
 // Fetch and update schedule data
 function timelineFun(data, membersData) {
+  let eventDay1 = document.querySelector("#event-Day1");
   let eventDay2 = document.querySelector("#event-Day2");
+  let eventDay3 = document.querySelector("#event-Day3");
+  let eventDay4 = document.querySelector("#event-Day4");
+  let eventDay5 = document.querySelector("#event-Day5");
+  let eventDay6 = document.querySelector("#event-Day6");
+
+  const eventDays = [
+    eventDay1,
+    eventDay2,
+    eventDay3,
+    eventDay4,
+    eventDay5,
+    eventDay6,
+  ];
 
   for (let i = 0; i < data.length; i++) {
     let member = membersData.filter(
@@ -17,7 +31,8 @@ function timelineFun(data, membersData) {
         .map((obj) => obj.member_name),
     ];
 
-    let eventDate = data[i].start_date;
+    let eventStartDate = data[i].start_date;
+    let eventEndDate = data[i].end_date;
 
     let time = (date) => {
       if (+date.slice(0, 2) > 12)
@@ -25,41 +40,41 @@ function timelineFun(data, membersData) {
       return `${date} am`;
     };
 
-    let start_time = time(eventDate.slice(11, 16));
-    let end_time = time(data[i].end_date.slice(11, 16));
+    let start_time = time(eventStartDate.slice(11, 16));
+    let end_time = time(eventEndDate.slice(11, 16));
 
     const eventScheduleHTML = `
-      <div class="col-md-3" id="event-schedule">
+    <div class="col-md-3" id="event-schedule">
         <div class="schedule-time mb-4">
-          <p>${start_time} - ${end_time}</p>
+        <p>${start_time} - ${end_time}</p>
         </div>
         <div class="schedule-place">
-          <p><a href="${data[i].meet_link}" target="_blank">Meet Joining Link</a></p>
+        <p><a href="${data[i].meet_link}" target="_blank">Meet Joining Link</a></p>
         </div>
-      </div>
-    `;
+        </div>
+        `;
     const eventDetailsHTML = `
       <div class="col-md-9 mt-4 mt-md-0" id="event-details">
         <h5>${data[i].name}</h5>
         <p><em>Hosted by ${data[i].type} Club</em></p>
         <div class="section pl-5">
-          <h6 class="mt-5 mb-3">${data[i].description}</h6>
+        <h6 class="mt-5 mb-3">${data[i].description}</h6>
           <p><strong>Cordinators</strong> - ${coOrdinator}</p>
           <p><strong>Co-Cordinators</strong> - ${coCoOrdinator}</p>
           <a href="${data[i].doc_link}" class="btn-pass-4"><button class="View_details_button">
             Get More Info
           </button> </a>
         </div>
-      </div>
+        </div>
     `;
 
-    if (eventDate.slice(0, 10) == "2021-10-03") {
-      eventDay2.insertAdjacentHTML("beforebegin", eventScheduleHTML);
-      eventDay2.insertAdjacentHTML("beforebegin", eventDetailsHTML);
+    function insertFunction(event) {
+      event.insertAdjacentHTML("afterend", eventDetailsHTML);
+      event.insertAdjacentHTML("afterend", eventScheduleHTML);
     }
-    if (eventDate.slice(0, 10) == "2021-10-04") {
-      eventDay2.insertAdjacentHTML("afterend", eventDetailsHTML);
-      eventDay2.insertAdjacentHTML("afterend", eventScheduleHTML);
+
+    for (let i = 0; i < eventDays.length; i++) {
+      if (+eventStartDate.slice(9, 10) == i + 3) insertFunction(eventDays[i]);
     }
   }
 }
